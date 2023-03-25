82529b54e3c6e1cc950d728f109f22163c4aeda0
wilfredoarroyo <warroyotola@gamil.com>
2023-03-24 23:31:56 -0400

    Clase Auxiliar Adicional

---
+++ Clases_herencia/Auxiliar.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Clases_herencia
+{
+    internal class Auxiliar : Usuario
+    {
+        public decimal Sueldo { get; set; }
+
+        public int Hora { get; set; }
+    }
+}
